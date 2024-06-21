50aa78288cf3f7116c717dbbfb7104988aa3729b
Datamwin <jethrod22@gmail.com>
2024-06-21 11:30:03 +0000

    Cleaning up the codes'

--- run_docker.sh
+++ run_docker.sh
@@ -1,11 +1,7 @@
-#!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
 docker build --tag=datamwin/app .
-# Step 2: 
+
 # List docker images
 docker image ls
 # Step 3: 
